afad1ff961b8a5d57b7d608afb48ba50d63b8ec9
LambdaXymox <lambda.xymox@gmail.com>
2016-02-17 01:21:23 -0600

    Remove test_paging function.

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -113,40 +113,6 @@ impl ActivePageTable {
 }
 
 
-pub fn test_paging<A>(allocator: &mut A)
-    where A: FrameAllocator
-{
-    let mut page_table = unsafe { ActivePageTable::new() };
-
-    // address 0 is mapped
-    println!("Some = {:?}", page_table.translate(0));
-    // second P1 entry
-    println!("Some = {:?}", page_table.translate(4096));
-    // second P2 entry
-    println!("Some = {:?}", page_table.translate(512 * 4096));
-    // 300th P2 entry
-    println!("Some = {:?}", page_table.translate(300 * 512 * 4096));
-    // second P3 entry
-    println!("None = {:?}", page_table.translate(512 * 512 * 4096));
-    // last mapped byte
-    println!("Some = {:?}", page_table.translate(512 * 512 * 4096 - 1));
-
-    let addr = 42 * 512 * 512 * 4096; // 42th P3 entry
-    let page = Page::containing_address(addr);
-    let frame = allocator.allocate_frame().expect("no more frames");
-    println!("None = {:?}, map to {:?}", page_table.translate(addr), frame);
-    page_table.map_to(page, frame, EntryFlags::empty(), allocator);
-    println!("Some = {:?}", page_table.translate(addr));
-    println!("next free frame: {:?}", allocator.allocate_frame());
-
-    page_table.unmap(Page::containing_address(addr), allocator);
-    println!("None = {:?}", page_table.translate(addr));
-    println!("{:#x}", unsafe {
-        *(Page::containing_address(addr).start_address() as *const u64)
-    });
-
-}
-
 pub struct InactivePageTable {
     p4_frame: Frame,
 }
